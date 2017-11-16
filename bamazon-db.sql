eb741c69d0385314ad4257592ae5678e4e26d48e
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-16 00:27:23 -0800

    Adding packages, js file, and mysql file

---
+++ bamazon-db.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	item_id INTEGER(100) NOT NULL,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(50) NULL,
+    price INTEGER(100) NOT NULL,
+    stock_quantity INTEGER(100)
+);
+
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (420, "Gemini Link Purse", "Women's Goods", 390.00, 50); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (630, "Playstation 4", "Electronics", 369.99, 250); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (557, "Face Oil", "Cosmetics", 40.00, 50); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (314, "Nintendo Switch", "Electronics", 299.99, 300); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (173, "Canon EOS 5D Mark IV", "Electronics", 3500.00, 150); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (666, "New Balance Black Shoes", "Men's Footware", 59.99, 50); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (148, "All Natural: Calcium", "Supplements/Nutrition", 6.99, 500); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (425, "Tory Burch Red Purse", "Women's Goods", 328.00, 50); 
+
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (426, "Michael Kors Pink Purse", "Women's Goods", 200.00, 50); 
+    
+INSERT INTO bamazon_db.products 
+	(item_id, product_name, department_name, price, stock_quantity)
+    VALUES (325, "XBOX One Black Controller", "Electronics", 39.99, 200);
+
