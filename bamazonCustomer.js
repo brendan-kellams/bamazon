ba13323e07ba76f6fe67b3bd925b55eff49b02df
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-16 01:24:09 -0800

    updated the connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,16 @@
 var inquirer = require('inquirer');
 var mySQL = require('mysql');
+
+var connection = mySQL.createConnection({
+    host: "LocalTacoShop",
+    port: 3306,
+    user: "root",
+    password:"",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+});
